6244162a450b32f0a969ab030749959b0a9fd2aa
amadridlar <amadridlar@gmail.com>
2023-11-06 19:20:54 +0100

    modify page

--- js/controllers/startNewGame.js
+++
@@ -1,1 +0,0 @@
-import{clearScore}from"../lib/options.js";import{changeTextFirstRow,changeTextSecondRow}from"../views/resultText.js";import updateScore from"../views/updateScore.js";const startNewGame=()=>{clearScore(),changeTextFirstRow(window.resultTextInitialMessage),changeTextSecondRow(""),updateScore(),void 0};export default startNewGame;
--- js/lib/computer.js
+++
@@ -1,2 +0,0 @@
-const computer={computerChoice:()=>{const a=Math.floor(3*Math.random());//NOSONAR
-return{0:"rock",1:"paper",2:"scissors"}[a]}};export default computer;
